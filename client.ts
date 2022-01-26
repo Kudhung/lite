64420c7bdb2328d70e7aaf6e4ac3c7bcd68dc07a
Kudhung <kudhung.bwi@gmail.com>
2022-01-26 16:29:22 +0700

    lite 2

--- client.ts
+++ client.ts
@@ -1,31 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-declare module NodeJS {
-    interface Global {
-        spotConfig: any
-    }
+declare global {
+    var prisma: PrismaClient | undefined
 }
-interface CustomNodeJSGlobal extends NodeJS.Global {
-    prisma: PrismaClient;
-}
-declare const global: CustomNodeJSGlobal;
+
 const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
-export default prisma;
-
-// import { PrismaClient } from '@prisma/client'
-
-// let prisma
-
-// if (process.env.NODE_ENV === 'production') {
-//     prisma = new PrismaClient()
-// } else {
-//     if (!global.prisma) {
-//         global.prisma = new PrismaClient()
-//     }
-//     prisma = global.prisma
-// }
-
-// export default prisma
-
+export default prisma
